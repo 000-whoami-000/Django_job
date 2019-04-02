a173e3b0674f0e089d55531ccd4a323f165fb3d9
PaulDzelendzyak <48528416+PaulDzelendzyak@users.noreply.github.com>
2019-04-02 13:28:31 +0300

    my first jon on django

---
+++ blog/frontSide/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+	path('',views.index,name="index"),
+	url(r'^Post(?P<id>[0-9]+)/$',views.Post,name="Post")
+]
