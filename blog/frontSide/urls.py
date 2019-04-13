3e3088eb6e36cef91922b4674178e1c129e0d7ae
PaulDzelendzyak <48528416+PaulDzelendzyak@users.noreply.github.com>
2019-04-13 18:39:50 +0300

    Error fixed

--- blog/frontSide/urls.py
+++ blog/frontSide/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
+
+# router=routers.DefaultRouter()
+# router.register(r'users',UserViewSet)
 urlpatterns = [
 	path('',views.index,name="index"),
 	url(r'^Post(?P<id>[0-9]+)/$',views.Post,name="Post")
+	# url(r'',include(router.urls))
 ]
--- blog/frontSide/views.py
+++ blog/frontSide/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+# return render(request, 'index.html',{'posts':posts})
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from frontSide.serializers import UserSerializer, GroupSerializer
 
 def index(request):
 	posts = Post.objects.all()
-	return render(request, 'index.html',{'posts':posts})
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
 
 	
 
